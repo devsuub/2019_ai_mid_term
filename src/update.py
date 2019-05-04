f2325ce2b3514953f90e6d3a05ea313425654252
minsub kim <minsubkim94@gmail.com>
2019-05-05 00:07:34 +0900

    update.py 추가

---
+++ src/update.py
@@ -0,0 +1,13 @@
+import sqlite3
+con = sqlite3.connect(':memory:')
+
+winningLine = []
+
+def update_people_play(position):
+    cur = con.cursor()
+    cur.execute("UPDATE WINNING_LINE SET CPU = 0 WHERE LINE LIKE :position", {"position": "%" + position + "%"})
+
+
+def update_ai_play(position):
+    cur = con.cursor()
+    cur.execute("UPDATE WINNING_LINE SET PEOPLE = 0 WHERE LINE LIKE :position", {"position": "%" + position + "%"})
